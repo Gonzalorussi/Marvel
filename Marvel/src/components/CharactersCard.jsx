fc22cabb1276363ca8e086dbf7ef83ac726cc001
marcelobuzzi <marcelobuzziriera@gmail.com>
2024-07-04 13:21:57 -0300

    Se agregó header y footer y cambio de estilos con bootstrap

--- Marvel/src/components/CharactersCard.jsx
+++ Marvel/src/components/CharactersCard.jsx
@@ -12,7 +12,7 @@ export const CharactersCard = ({charactersMap})=>{
                 <Link to={`/character/${charactersMap.id}`} className="my-efect-card card text-bg-dark mx-auto" style={{width: '18rem'}}>
                     <img src={imgUrl} className="card-img" alt={charactersMap.name}/>
                     <div className="card-img-overlay">
-                        <h5 className="card-title">{charactersMap.name}</h5>
+                        <h5 className="card-title text-light text-shadow">{charactersMap.name}</h5>
                     </div>
                 </Link>
             </div>
